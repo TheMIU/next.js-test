5cf5c70f09661657411a877d14dd4bd812d73bc6
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-03-17 17:25:33 +0530

    add css

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Link from "next/link";
+import styles from "@/app/contact/contact-styles.module.css"
 
 function page() {
   return (
     <>
-      <h1>contact us page</h1>
-      <li> <Link href="/">Home</Link> </li>
+      {/* add styles  */}
+
+      {/* using id selector */}
+      <h1 id={styles.heading} >contact us page</h1>
+
+      {/* using class selector */}
+      <li> <Link href="/" className={styles.link}>Home</Link> </li>
     </>
   )
 }
